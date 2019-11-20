250e287273bbc7d8c978ce1bbb3490868f47b749
snehasish <snehasish.rath@gmail.com>
2019-11-20 16:35:48 +0100

    html

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 // load the http module
 var http = require('http');
+fs = require('fs');
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err;
+    }
 
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
     response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("Hello world new\n");
+//    response.end("Hello world new\n");
+    response.write(html);
+    response.end();
 });
 
 // listen on localhost:8000
