2356b7c6a9c7459fe718f825544174bf603dbbd2
snehasish <snehasish.rath@gmail.com>
2019-11-20 16:40:55 +0100

    hjh

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // load the http module
-var http = require('http');
-fs = require('fs');
+var http = require('http'),
+    fs = require('fs');
 
 fs.readFile('./index.html', function (err, html) {
     if (err) {
